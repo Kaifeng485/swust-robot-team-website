01a710baa2bd9b90df5121a4d7654e79057a9cc7
李世民 <2197974202@qq.com>
2026-07-30 15:27:38 +0800

    Convert website for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+const repositoryBasePath = "/swust-robot-team-website";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGitHubPages ? repositoryBasePath : "",
+  assetPrefix: isGitHubPages ? repositoryBasePath : "",
 };
 
 export default nextConfig;
